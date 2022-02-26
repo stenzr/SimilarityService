3d2180e41ee4351e33071fcefdc2152456cce325
stenzr <rkrohitkr01@gmail.com>
2022-02-27 00:29:44 +0530

    [ADD] method to generate the response json

--- App/app.py
+++ App/app.py
@@ -13,6 +13,9 @@ users = db["Users"]
 
 
 class Utility:
+    def __init__(self):
+        self.response = {}
+
     def UserExist(self, username):
         if users.find({"Username": username}).count() == 0:
             return False
@@ -39,6 +42,14 @@ class Utility:
 
         return tokens
 
+    def get_response_json(self, status=501, msg="Internal Error", similarity=None, currentTokens=None):
+        self.response["status"] = status
+        self.response["msg"] = msg
+        self.response["similarity"] = similarity
+        self.response["currentTokens"] = currentTokens
+
+        return self.response
+
 
 class Register(Resource):
     def post(self):
@@ -49,11 +60,8 @@ class Register(Resource):
         password = postedData["password"]
 
         if utility.UserExist(username):
-            retJson = {
-                "status": 301,
-                "msg": "Invalid Username"
-            }
-
+            retJson = utility.get_response_json(
+                status=301, msg="Invalid Username")
             return jsonify(retJson)
 
         hashed_pw = bcrypt.hashpw(password.encode("utf8"), bcrypt.gensalt())
@@ -64,10 +72,9 @@ class Register(Resource):
             "Tokens": 6
         })
 
-        retJson = {
-            "status": 200,
-            "msg": "Sign up Successful"
-        }
+        retJson = utility.get_response_json(
+            status=200, msg="Sign up Successful")
+        return jsonify(retJson)
 
 
 class Detect(Resource):
@@ -81,31 +88,22 @@ class Detect(Resource):
         text2 = postedData["text2"]
 
         if not utility.UserExist(username):
-            retJson = {
-                "status": 301,
-                "msg": "Invalid Username"
-            }
-
+            retJson = utility.get_response_json(
+                status=301, msg="Invalid Username")
             return jsonify(retJson)
 
         correct_pw = utility.verifyPw(username, password)
 
         if not correct_pw:
-            retJson = {
-                "status": 302,
-                "msg": "Invalid Password"
-            }
-
+            retJson = utility.get_response_json(
+                status=302, msg="Invalid Password")
             return jsonify(retJson)
 
         num_tokens = utility.countTokens(username)
 
         if num_tokens <= 0:
-            retJson = {
-                "status": 303,
-                "msg": "You have run out of tokens. Please refill"
-            }
-
+            retJson = utility.get_response_json(
+                status=303, msg="You have run out of tokens. Please refill")
             return jsonify(retJson)
 
         nlp = spacy.load("en_core_web_sm")
@@ -115,11 +113,9 @@ class Detect(Resource):
 
         ratio = text1.similarity(text2)
 
-        retJson = {
-            "status": 200,
-            "similarity": ratio*100,
-            "msg": "Success"
-        }
+        similarity_score = ratio*100
+        retJson = utility.get_response_json(
+            status=200, msg="Success", similarity=similarity_score)
 
         current_tokens = utility.countTokens(username)
 
@@ -133,6 +129,7 @@ class Detect(Resource):
 
         return jsonify(retJson)
 
+
 class Refill(Resource):
     def post(self):
         utility = Utility()
@@ -143,21 +140,15 @@ class Refill(Resource):
         refill_amount = postedData["refill"]
 
         if not utility.UserExist(username):
-            retJson = {
-                "status": 301,
-                "msg": "Invalid Username"
-            }
-
+            retJson = utility.get_response_json(
+                status=301, msg="Invalid Username")
             return jsonify(retJson)
 
         correct_pw = utility.verifyPw(username="admin", password=password)
 
         if not correct_pw:
-            retJson = {
-                "status": 304,
-                "msg": "Invalid Admin Password"
-            }
-
+            retJson = utility.get_response_json(
+                status=304, msg="Invalid Admin Password")
             return jsonify(retJson)
 
         current_tokens = utility.countTokens(username)
@@ -172,13 +163,10 @@ class Refill(Resource):
             }
         })
 
-        retJson = {
-            "status": 200,
-            "currentTokens": updated_tokens,
-            "msg": "Success"
-        }        
-
+        retJson = utility.get_response_json(
+            status=200, msg="Success", currentTokens=updated_tokens)
         return jsonify(retJson)
+
 
 api.add_resource(Register, "/register")
 api.add_resource(Detect, "/detect")
@@ -186,5 +174,3 @@ api.add_resource(Refill, "/refill")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-    
-
