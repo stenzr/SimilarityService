132f4e8484a09a19be95b0afc4de2e0797d868b4
stenzr <rkrohitkr01@gmail.com>
2022-02-27 00:09:03 +0530

    [ADD] api endpoint routes

--- App/app.py
+++ App/app.py
@@ -180,3 +180,11 @@ class Refill(Resource):
 
         return jsonify(retJson)
 
+api.add_resource(Register, "/register")
+api.add_resource(Detect, "/detect")
+api.add_resource(Refill, "/refill")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    
+
